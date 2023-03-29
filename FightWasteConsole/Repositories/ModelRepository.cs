bf2041f18e2258a0254624d752b3ae51a3a90fc3
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2023-03-29 22:20:42 +0100

    Created IRepository interface to abstract data access, wrote tests for a ModelRepository class

---
+++ FightWasteConsole/Repositories/ModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using FightWasteConsole.DataAccess;
+
+namespace FightWasteConsole.Repositories;
+
+public class ModelRepository<IModel> : IRepository<IModel>
+{
+    private readonly IDataAccess<IModel> _dataAccess;
+
+    public ModelRepository(IDataAccess<IModel> dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+
+    public IEnumerable<IModel> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
