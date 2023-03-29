015a896fb81f07e66c2d49dbf707a5ba765712b5
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2023-03-29 20:45:18 +0100

    Implemented JsonDataAccess.GetData() added another unit test to ensure model returned from JSON is equivalent to an expected model

--- FightWasteConsole/DataAccess/JsonDataAccess.cs
+++ FightWasteConsole/DataAccess/JsonDataAccess.cs
@@ -7,6 +7,7 @@ public class JsonDataAccess<IModel> : IDataAccess<IModel>
 {
     private readonly string _filePath;
 
+    // HACK: This should eventually be replaceed by real database access
     public JsonDataAccess(string filePath)
     {
         _filePath = filePath;
@@ -14,6 +15,10 @@ public class JsonDataAccess<IModel> : IDataAccess<IModel>
 
     public IEnumerable<IModel> GetData()
     {
-        throw new NotImplementedException();
+        // TODO: Getting Unit enum value from a string in the JSON, instead of
+        //  a numerical representation 
+        var fileStream = File.ReadAllText(_filePath);
+        var items = JsonSerializer.Deserialize<List<IModel>>(fileStream);
+        return items.AsEnumerable();
     }
 }
--- FightWasteConsoleTests/DataAccess/JsonDataAccessTests.cs
+++ FightWasteConsoleTests/DataAccess/JsonDataAccessTests.cs
@@ -1,23 +1,54 @@
 ﻿using FightWasteConsole.DataAccess;
 using FightWasteConsole.Models;
-using System.Configuration;
 
 namespace FightWasteConsoleTests.DataAccess;
 
 [TestFixture]
 public class JsonDataAccessTests
 {
-    [Test]
-    public void GetDataReturnsRealData()
-    {
-        // Arrange
-        var jsonDataAccess = new JsonDataAccess<MealModel>
-            ($"{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}..\\..\\..\\DataAccess\\TestMealModels.json");
+    private JsonDataAccess<MealModel> _jsonDataAccess;
 
+    [SetUp]
+    public void SetUp()
+    {
+        _jsonDataAccess = new
+            ($"{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}..\\..\\..\\DataAccess\\TestMealModels.json");
+    }
+
+    [Test]
+    public void GetDataReturnsModelFromFile()
+    {
         // Act
-        var data = jsonDataAccess.GetData();
+        var actual = _jsonDataAccess.GetData();
 
         // Assert
-        data.Should().BeOfType<List<MealModel>>();
+        actual.Should().BeOfType<List<MealModel>>();
     }
+
+    [Test]
+    public void GetDataReturnsDataAsModel()
+    {
+        // Arrange
+        var expected = GetExpectedMealModels();
+
+        // Act
+        var actual = _jsonDataAccess.GetData();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    private List<MealModel> GetExpectedMealModels() => new()
+    {
+        new()
+        {
+            Id = 1,
+            Name = "Test Meal",
+            Ingredients = new()
+            {
+                new() { Name = "Mushrooms", Quantity = 50, Unit = Unit.G },
+                new() { Name = "Watermelon", Quantity = 1, Unit = Unit.Of }
+            }
+        }
+    };
 }
