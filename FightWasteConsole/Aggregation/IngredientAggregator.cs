50ae8d418bcc085b0bfeb28c3b2217c971bc13fa
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2023-03-30 22:20:15 +0100

    Implemented IngredientAggregator.CombineIngredients() method

--- FightWasteConsole/Aggregation/IngredientAggregator.cs
+++ FightWasteConsole/Aggregation/IngredientAggregator.cs
@@ -6,6 +6,27 @@ public class IngredientAggregator : IIngredientAggregator
 {
     public IEnumerable<IngredientQuantityModel> CombineIngredients(IEnumerable<IngredientQuantityModel> ingredients)
     {
-        throw new NotImplementedException();
+        var uniqueIngredientNames = ingredients
+            .Select(ingredient => ingredient.Name)
+            .Distinct();
+
+        var ingredientsToReturn = new List<IngredientQuantityModel>();
+
+        uniqueIngredientNames.ToList().ForEach(name =>
+        {
+            var totalQuantity = ingredients
+                .Where(ingredient => ingredient.Name == name)
+                .Select(ingredient => ingredient.Quantity)
+                .Sum();
+
+            ingredientsToReturn.Add(new IngredientQuantityModel
+            {
+                Name = name,
+                Quantity = totalQuantity,
+                Unit = ingredients.First(a => a.Name == name).Unit
+            });
+        });
+
+        return ingredientsToReturn;
     }
 }
