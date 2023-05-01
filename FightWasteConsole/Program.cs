b8ad23ad4d62301bb1515db4164e5411855b0765
Alistair Tait <alistair.tait1@gmail.com>
2023-05-01 20:38:27 +0100

    Added configuration to get settings from FightWasteOptions section of appsettings.Production.json

--- FightWasteConsole/Program.cs
+++ FightWasteConsole/Program.cs
@@ -5,14 +5,25 @@ using FightWasteConsole.DataAccess;
 using FightWasteConsole.FileWriter;
 using FightWasteConsole.IngredientsListProcessing;
 using FightWasteConsole.Models;
+using FightWasteConsole.Options;
 using FightWasteConsole.Output;
 using FightWasteConsole.Repositories;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostContext, configuration) =>
+    {
+        configuration.Sources.Clear();
+        IHostEnvironment env = hostContext.HostingEnvironment;
+        configuration.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+    })
     .ConfigureServices((hostContext, services) =>
     {
+        var configurationRoot = hostContext.Configuration;
+        services.Configure<FightWasteOptions>(configurationRoot.GetSection(nameof(FightWasteOptions)));
+
         services.AddHostedService<FightWasteHost>();
         services.AddTransient<IIngredientsListProcessor, IngredientsListProcessor>()
             .AddTransient<IConsoleWrapper, ConsoleWrapper>()
@@ -21,7 +32,6 @@ IHost host = Host.CreateDefaultBuilder(args)
             .AddTransient<IFileWriter, TableFileWriter>()
             .AddTransient<IIngredientAggregator, IngredientAggregator>()
             .AddTransient<IDataAccess<MealModel>, JsonDataAccess<MealModel>>();
-
     }).Build();
 
 host.Run();
