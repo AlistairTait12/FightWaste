44b55d3efe90cd02ae927f50a3f0f188da396f2a
Alistair Tait <alistair.tait1@gmail.com>
2023-05-03 23:28:49 +0100

    Added unit test for showall command to display all meals in the repo

---
+++ FightWasteConsole/Commands/ShowAllMealsCommand.cs
@@ -0,0 +1,25 @@
+﻿using FightWasteConsole.ConsoleWrapper;
+using FightWasteConsole.DataAccess;
+using FightWasteConsole.Models;
+using FightWasteConsole.Repositories;
+
+namespace FightWasteConsole.Commands;
+
+public class ShowAllMealsCommand : ICommand
+{
+    private readonly IMealRepository _mealRepository;
+    private readonly IConsoleWrapper _consoleWrapper;
+
+    public ShowAllMealsCommand(IMealRepository mealRepository, IConsoleWrapper consoleWrapper)
+    {
+        _mealRepository = mealRepository;
+        _consoleWrapper = consoleWrapper;
+    }
+
+    public IEnumerable<string> Aliases => throw new NotImplementedException();
+
+    public void Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
