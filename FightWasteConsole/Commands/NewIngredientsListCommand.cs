98539a87066bf1b5d12ee5a30bb438337b25fc07
Alistair Tait <alistair.tait1@gmail.com>
2023-05-03 21:51:39 +0100

    Added unit tests for and added NewIngredientsListCommand

---
+++ FightWasteConsole/Commands/NewIngredientsListCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace FightWasteConsole.Commands;
+
+public class NewIngredientsListCommand : ICommand
+{
+    public IEnumerable<string> Aliases => throw new NotImplementedException();
+
+    public void Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FightWasteConsoleTests/Commands/NewIngredientsListCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using FightWasteConsole.Commands;
+using FightWasteConsole.IngredientsListProcessing;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FightWasteConsoleTests.Commands;
+
+[ExcludeFromCodeCoverage]
+[TestFixture]
+public class NewIngredientsListCommandTests
+{
+    private IIngredientsListProcessor _processor;
+    private NewIngredientsListCommand _newIngredientsListCommand;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _processor = A.Fake<IIngredientsListProcessor>();
+        _newIngredientsListCommand = new NewIngredientsListCommand();
+    }
+
+    // TODO Test that a call to Fake IIngredientsListProcessor.ProduceIngredientsList()
+    // was called when correct command is called
+    [Test]
+    public void ExecuteCallsProduceIngredientsList()
+    {
+        // Act
+        _newIngredientsListCommand.Execute();
+
+        // Assert
+        A.CallTo(() => _processor.ProduceIngredientsList()).MustHaveHappenedOnceExactly();
+    }
+}
