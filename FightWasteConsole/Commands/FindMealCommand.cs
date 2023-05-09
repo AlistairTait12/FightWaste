31c25bf63b949974274ed574ab1d3a799f76248c
Alistair Tait <alistair.tait1@gmail.com>
2023-05-09 23:18:30 +0100

    Committing in broken state to save progress

--- FightWasteConsole/Commands/FindMealCommand.cs
+++ FightWasteConsole/Commands/FindMealCommand.cs
@@ -1,13 +1,30 @@
 ﻿using FightWasteConsole.CommandArguments;
+using FightWasteConsole.ConsoleWrapper;
+using FightWasteConsole.Repositories;
 
 namespace FightWasteConsole.Commands;
 
 public class FindMealCommand : ICommand
 {
+    private readonly IMealRepository _mealRepository;
+    private readonly IConsoleWrapper _consoleWrapper;
+    private readonly List<string> _aliasesForNameArg = new() { "name", "n" };
+
+    public FindMealCommand(IMealRepository mealRepository, IConsoleWrapper consoleWrapper)
+    {
+        _mealRepository = mealRepository;
+        _consoleWrapper = consoleWrapper;
+    }
+
     public IEnumerable<string> Aliases => throw new NotImplementedException();
 
     public void Execute(List<Argument> arguments)
     {
-        throw new NotImplementedException();
+        var nameArg = arguments.FirstOrDefault(arg => _aliasesForNameArg.Contains(arg.ArgumentName));
+
+        if (nameArg is null)
+        {
+            _consoleWrapper.Warn($"Argument `{}`");
+        }
     }
 }
